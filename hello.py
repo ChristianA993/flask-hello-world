d2081fe16b8ebee1da5026a40736e07583b3e3b8
Zephyrus <adam.christjan@gmail.com>
2024-01-16 20:03:18 +0100

    We updated the Hello world respons text and installed modules

--- hello.py
+++ hello.py
@@ -1,11 +1,10 @@
-# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World! Now my app is running locally! '
 
 if __name__ == '__main__':
     app.run()
